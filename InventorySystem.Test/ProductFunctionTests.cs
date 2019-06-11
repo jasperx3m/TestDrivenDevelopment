5c542588e7a3cabf8cf98b45bd29f684a013a4c7
orilla-jasper <jasper.orilla915@gmail.com>
2019-06-11 15:51:26 +0800

    Add project files.

---
+++ InventorySystem.Test/ProductFunctionTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventorySystem;
+using Models;
+using Xunit;
+
+namespace InventorySystem.Test
+{
+    public class ProductFunctionTest
+    {
+        [Fact]
+        public void ViewProducts_ShouldWork()
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 = new Product { ProductID = 1002, ProductName = "Tabler", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+            ProductFunction.AddProduct(productlist, product1);
+            ProductFunction.AddProduct(productlist, product2);
+
+            Assert.True(productlist.Count == 2);
+        }
+        [Fact]
+        public void AddProduct_ShouldWork()
+        {
+            Product product = new Product { ProductID = 1001, ProductName = "Chair", Quantity=22 };
+            List<Product> productlist = new List<Product>();
+
+            ProductFunction.AddProduct(productlist, product);
+            Assert.True(productlist.Count == 1);
+            Assert.Contains<Product>(product, productlist);
+
+        }
+
+        [Theory]
+        [InlineData(null,"Chair",22,"Product ID")]//null id
+        [InlineData(int.MaxValue,"Chair",null,"Product Quantity")]//Negative quantity
+        [InlineData(1001,"",22,"Product Name")]//null productname
+        [InlineData(-100,"Chair",15,"Product ID")]//Decimal ID
+        [InlineData(10002, "Chair",-123,"Product Quantity")]//Negative Quantity
+        [InlineData(12, "ChairChairChairChairChairChairChairChairChairChairChairChairChairChairChairChairChairChair",15,"ProductName")]//More than 20 characters
+        [InlineData(12, "Ch", 15, "Product Name")]
+        public void AddProduct_ShouldFail(int productid,string productname,int quantity,string param)
+        {
+            Product product = new Product { ProductID = productid, ProductName =productname, Quantity =quantity };
+            List<Product> productlist = new List<Product>();
+            Assert.Throws<ArgumentException>(param, () => ProductFunction.AddProduct(productlist, product));
+        }
+        [Fact]
+        public void AddProductDuplicateProductID_ShouldFail()
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 = new Product { ProductID = 1001, ProductName = "Table", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+
+            ProductFunction.AddProduct(productlist, product1);
+            Assert.Throws<ArgumentException>(() => ProductFunction.AddProduct(productlist, product2));
+            Assert.True(productlist.Count == 1);
+        }
+        [Fact]
+        public void RemoveProduct_ShouldWork()
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 =new Product { ProductID = 1002, ProductName = "Tabler", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+            ProductFunction.AddProduct(productlist, product1);
+            ProductFunction.AddProduct(productlist, product2);
+            ProductFunction.RemoveProduct(productlist, product1.ProductID);
+            Assert.True(productlist.Count == 1);
+            Assert.DoesNotContain(product1, productlist);
+        }
+        [Fact]
+        public void RemoveProduct_ShouldFail()
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 = new Product { ProductID = 1002, ProductName = "Tabler", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+            ProductFunction.AddProduct(productlist, product1);
+            ProductFunction.AddProduct(productlist, product2);
+            Assert.Throws<ArgumentException>(() => ProductFunction.RemoveProduct(productlist, 10003));
+
+        }
+        [Fact]
+        public void EditProductByID_ShouldWork()
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 = new Product { ProductID = 1002, ProductName = "Table", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+            ProductFunction.AddProduct(productlist, product1);
+            ProductFunction.AddProduct(productlist, product2);
+            Product product_edit = new Product { ProductID = 1001, ProductName = "Sofa", Quantity = 25 };
+            Assert.True(ProductFunction.EditProductByID(productlist, 1001, product_edit));
+        }
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { 1001,1002, "Sofa", int.MaxValue },//duplicate id
+            new object[] {99,int.MaxValue,"Chair",-23},// id does not exist
+            new object[] {null,1000,"Chair",15},//null reference id
+            new object[] {1001,1000,"",15},//null productname
+            new object[] {1001,null,"Sofa",15},//null Product ID
+            new object[] {1001,1000,"Chair",null},//null Quantity
+            new object[] {1001,int.MinValue,"Chair",23},//negative id
+            new object[] {1001,int.MaxValue,"Chair",-23},//negative quantity
+            new object[] {1001,1001,"Ch",15},//Name less than 3 characters
+            new object[] {1001,1001, "ChairSofaChairSofaChairSofaChairSofaChairSofaChairSofaChairSofa", 75},//Name is greater than 20 characters
+
+        };
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void EditProductByID_ShouldFail(int productid,int newproductid,string productname, int quantity)
+        {
+            Product product1 = new Product { ProductID = 1001, ProductName = "Chair", Quantity = 22 };
+            Product product2 = new Product { ProductID = 1002, ProductName = "Table", Quantity = 15 };
+            List<Product> productlist = new List<Product>();
+            ProductFunction.AddProduct(productlist, product1);
+            ProductFunction.AddProduct(productlist, product2);
+            Product product_edit = new Product { ProductID = newproductid, ProductName = productname, Quantity = quantity };
+            Assert.Throws<ArgumentException>(() => ProductFunction.EditProductByID(productlist,productid, product_edit));
+
+
+        }
+
+
+
+
+    }
+}
---
+++ InventorySystem/ProductFunction.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace InventorySystem
+{
+    public class ProductFunction
+    {
+        public static List<Product> ViewProducts(List<Product> productlist)
+        {
+            return productlist;
+        }
+        public static void AddProduct(List<Product> productlist,Product product)
+        {
+            bool uniqueID = false;
+            if (productlist.Count == 0)
+            {
+                uniqueID = true;
+            }
+            foreach(var checkID in productlist)
+            {
+                if (checkID.ProductID != product.ProductID)
+                {
+                    uniqueID = true;
+                }
+            }
+            if (uniqueID==true)
+            {
+                if (string.IsNullOrWhiteSpace(product.ProductName))
+                {
+                    throw new ArgumentException("You passed in an invalid parameter", "Product Name");
+                }
+                if (product.ProductID == 0)
+                {
+                    throw new ArgumentException("You passed a null value", "Product ID");
+                }
+                if (product.Quantity == 0)
+                {
+                    throw new ArgumentException("You passed a null value", "Product Quantity");
+                }
+                if (product.ProductID < 0)
+                {
+                    throw new ArgumentException("You passed a negative value", "Product ID");
+                }
+                if (product.Quantity < 0)
+                {
+                    throw new ArgumentException("You passed a negative value", "Product Quantity");
+                }
+                if (product.ProductName.Length < 3)
+                {
+                    throw new ArgumentException("Product Name was too short", "Product Name");
+                }
+                if (product.ProductName.Length > 20)
+                {
+                    throw new ArgumentException("Product Name was too long", "ProductName");
+                }
+                productlist.Add(product);
+            }
+            else
+            {
+                throw new ArgumentException("You passed an already used Product ID");
+            }
+            
+        }
+        public static void RemoveProduct(List<Product> productlist, int productID)
+        {
+            bool containsID = false;
+            foreach (var product in productlist)
+            {
+                if (product.ProductID == productID)
+                {
+                    containsID = true;
+                }
+            }
+            if (containsID == true)
+            {
+                var toDelete = from product in productlist
+                               where product.ProductID == productID
+                               select product;
+                productlist.RemoveAll(toDelete.Contains);
+            }
+            else
+            {
+                throw new ArgumentException("There is no such Product ID", "Product ID");
+            }
+
+         }
+        public static bool EditProductByID(List<Product> productlist,int id,Product product)
+        {
+            bool isAvailable = false;
+            bool isUnique = true;
+            foreach (var i in productlist)
+            {
+
+                if (i.ProductID == id)
+                {
+                    isAvailable = true;
+                }
+            }
+            foreach (var i in productlist.Where(p => p.ProductID != id))
+            {
+                if (product.ProductID==i.ProductID)
+                {
+                    isUnique = false;    
+                }
+            }
+            if (isUnique==false)
+            {
+                throw new ArgumentException("You passed an an already used ID");
+            }
+            
+            if (isAvailable ==true && isUnique==true)
+            {
+                if (string.IsNullOrWhiteSpace(product.ProductName))
+                {
+                    throw new ArgumentException("You passed in an invalid parameter");
+                }
+                if (product.ProductID == 0)
+                {
+                    throw new ArgumentException("You passed a null value");
+                }
+                if (product.Quantity == 0)
+                {
+                    throw new ArgumentException("You passed a null value");
+                }
+                if (product.ProductID < 0)
+                {
+                    throw new ArgumentException("You passed a negative value");
+                }
+                if (product.Quantity < 0)
+                {
+                    throw new ArgumentException("You passed a negative value");
+                }
+                if (product.ProductName.Length < 3)
+                {
+                    throw new ArgumentException("Product Name was too short");
+                }
+                if (product.ProductName.Length > 20)
+                {
+                    throw new ArgumentException("Product Name was too long");
+                }
+                foreach (var i in productlist.Where(p => p.ProductID == id))
+                {
+                    i.ProductID = product.ProductID;
+                    i.ProductName = product.ProductName;
+                    i.Quantity = product.Quantity;
+                }
+                return true;
+            }
+            
+            else
+            {
+                throw new ArgumentException("Product ID is not found");
+            }
+        }
+    }
+}
